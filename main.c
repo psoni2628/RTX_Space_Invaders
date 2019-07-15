63afa94bc9846f2b09722d7d5cf410251758207e
Prabhdeep Singh Soni <pssoni@uwaterloo.ca>
2019-07-15 17:31:54 -0400

    one bullet at a time

--- main.c
+++ main.c
@@ -6,7 +6,51 @@
 #include "Dependencies/random.h"
 #include "Bitmaps/bitmap.h"
 
-//  p = [int(0xFFFF/0xFF*i) for i in v]
+#define initialPositionY 0
+#define CENTERUSERX 106
+
+typedef struct{
+	uint8_t y,x;
+} ship;
+
+ship userShip = {0,CENTERUSERX};
+
+ship bullet = {26,120-1};
+bool bulletactive = false;
+	
+void userIO(void *arg){
+	while (true) {
+		uint32_t joystickLeft = (LPC_GPIO1->FIOPIN & 0x800000);
+		uint32_t joystickRight = (LPC_GPIO1->FIOPIN & 0x2000000);
+		uint32_t pushbutton = (LPC_GPIO2->FIOPIN & 0x400);
+		
+		if (!joystickLeft && userShip.x>0) {
+				userShip.x--;
+				printf("%d\n",userShip.x);
+		}
+		else if (!joystickRight && userShip.x<240-24){
+			userShip.x++;
+			printf("RIGHT,%d",userShip.x);
+		}
+		if(!pushbutton){
+			bulletactive = true;
+			bullet.x=userShip.x;
+			printf("posh");
+		}
+		
+	osDelay(osKernelGetTickFreq()*0.005);
+	}
+}
+
+void bulletMove(void *arg){
+		while(true){
+			if(bulletactive){
+				bullet.y++;
+				osDelay(osKernelGetTickFreq()*0.0005);
+			}
+				osThreadYield();
+		}
+}
 
 void displayLED(uint32_t num){
 	LPC_GPIO1->FIODIR |= 0xB0000000;
@@ -49,36 +93,21 @@ void displayLED(uint32_t num){
 }
 
 void GLCD_Display(void *arg) {
-	char c[1]="|";
 	GLCD_Init();
 	GLCD_Clear(Black);
 	GLCD_SetBackColor(Black);
-	GLCD_SetTextColor(White);
-	//for (int col = 0; col < 8; col++) {
-		
-	//}
-	
-	int i=0;
-	char i_c[10];
 	while (true) {
-		sprintf(i_c,"%d",i);
-		osDelay(osKernelGetTickFreq()*1);
-		//for (int col = 0; col < 8; col++) {
-			//GLCD_DisplayString(i,3,1,(unsigned char *)c);
-		//}
-		//GLCD_Bitmap(320-24,240-26,24,26,(unsigned char*)user_bm); //gets you to top right
-		GLCD_Bitmap(0,106,24,26,(unsigned char*)user_bm); //gets you to top right
+		GLCD_Bitmap(0,userShip.x,24,26,(unsigned char*)user_bm); //gets you to top right
+		GLCD_Bitmap(0,userShip.x+26,24,10,(unsigned char*)black); //gets you to top right
+		GLCD_Bitmap(0,userShip.x-10,24,10,(unsigned char*)black); //gets you to top right
 		GLCD_Bitmap(40,0,4,240,(unsigned char*)line);
-		GLCD_Bitmap(26,119,20,1,(unsigned char*)line);
+		GLCD_Bitmap(bullet.y,bullet.x,20,1,(unsigned char*)line); //bullet
+		GLCD_Bitmap(bullet.y+20,bullet.x,5,1,(unsigned char*)black); //gets you to top right
+		GLCD_Bitmap(bullet.y-5,bullet.x,5,1,(unsigned char*)black); //gets you to top right
 		GLCD_Bitmap(320-24,240-25,24,25,(unsigned char*)enemy_bm);
-		//GLCD_DisplayString(0,0,1,(unsigned char *)c);
-		//GLCD_DisplayString(6,8,1,(unsigned char *)i_c);
-		//if(i<10)
-		//	i++;
-		//if(i>10)
-		//	break;
+		displayLED((uint32_t)1);
+		osThreadYield();
 	}
-
 }
 
 int main(void) {
@@ -86,9 +115,8 @@ int main(void) {
 	SystemInit(); 
 	osKernelInitialize();
 	osThreadNew(GLCD_Display, NULL, NULL);
+	osThreadNew(userIO, NULL, NULL);
+	osThreadNew(bulletMove, NULL, NULL);
 	osKernelStart();
 	while(true);
-	
-	
-	
 }
